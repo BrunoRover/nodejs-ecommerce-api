bed79ce6e655219279efc34e764127669bc8983f
Bruno Dalbosco Rover <brunoplay055@gmail.com>
2025-01-07 11:31:17 -0300

    initial config to the database

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const app = express()
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 require("dotenv/config")
 const api = process.env.API_URL
 
 //middleware
 app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
 
 //http://localhost:3000/api/v1/products
 app.get(`${api}/products`, (req, res) =>{
@@ -22,7 +26,11 @@ app.post(`${api}/products`, (req, res) =>{
     console.log(newProducts)
     res.send(newProducts)
 })
-
+mongoose.connect(process.env.DB).then(()=>{
+    console.log("database is ready")
+}).catch((err)=>{
+    console.log(err)
+})
 app.listen(3000, ()=>{
     console.log("server is running http://localhost:3000")
 })
