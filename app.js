494a8eb90c264582a88be886afd3e33b69d742e6
Bruno Dalbosco Rover <brunoplay055@gmail.com>
2025-01-19 17:58:50 -0300

    Product Image & Gallery Upload

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use('*', cors())
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(authJwt());
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
 app.use(errorHandler);
 
 //Router
